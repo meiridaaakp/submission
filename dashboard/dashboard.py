b22308488b62391fd1bc8b3e750fc8323a03ae3c
meiridaaakp <meirida1652@gmail.com>
2024-10-02 16:37:59 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,13 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 from matplotlib.gridspec import GridSpec
 
+# Set seaborn style
 sns.set(style='dark')
 
 # Load dataset
-df_day = pd.read_csv("https://raw.githubusercontent.com/meiridaaakp/submission/refs/heads/main/dashboard/day_data")
+df_day_new = pd.read_csv("https://raw.githubusercontent.com/meiridaaakp/submission/refs/heads/main/dashboard/day_data")
 
 # Streamlit configuration
 st.set_page_config(page_title="BIKERS", page_icon="🚴‍♂️", layout="wide")
@@ -20,30 +20,60 @@ st.markdown("---")
 
 # Display dataset
 st.subheader("Dataset Jumlah Penyewa Sepeda")
-st.dataframe(df_day.head())
+st.dataframe(df_day_new.head())
 
-# Analysis of weather effect on bike rentals
-st.subheader("Pengaruh Cuaca Terhadap Penggunaan Sepeda")
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.barplot(x='weathersit', y='cnt', data=df_day, estimator=sum, palette='Set1', ax=ax)
-ax.set_title('Pengaruh Cuaca Terhadap Penggunaan Sepeda', fontsize=16)
-ax.set_xlabel('Situasi Cuaca', fontsize=12)
-ax.set_ylabel('Total Penggunaan Sepeda', fontsize=12)
-st.pyplot(fig)
+# Visualisasi penyewa sepeda berdasarkan "weathersit"
+st.subheader("Jumlah Penyewa Berdasarkan Cuaca")
+fig1 = plt.figure(figsize=(12, 10))
+gs1 = GridSpec(4, 4, fig1, wspace=0.5, hspace=0.5)
+plt.suptitle("Jumlah Penyewa Berdasarkan Cuaca", fontsize=16)
 
-# Comparison of bike usage on working days vs holidays
-st.subheader("Perbandingan Penggunaan Sepeda: Hari Kerja vs Hari Libur")
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.barplot(x='workingday', y='cnt', data=df_day, estimator=sum, palette='Set2', ax=ax)
-ax.set_title('Perbandingan Penggunaan Sepeda: Hari Kerja vs Hari Libur', fontsize=16)
-ax.set_xlabel('Hari Libur vs Hari Kerja', fontsize=12)
-ax.set_ylabel('Total Penggunaan Sepeda', fontsize=12)
-st.pyplot(fig)
+plt.subplot(gs1[:2, 1:3])
+sns.boxplot(x='weathersit', y='cnt', data=df_day_new, palette="Set2", hue='weathersit', legend=False)
+plt.xlabel("Cuaca", fontsize=12)
+plt.ylabel("Jumlah Penyewa", fontsize=12)
+
+plt.subplot(gs1[2:, :2])
+sns.boxplot(x='weathersit', y='casual', data=df_day_new, palette="Set2", hue='weathersit', legend=False)
+plt.xlabel("Cuaca", fontsize=12)
+plt.ylabel("Jumlah Penyewa Non-Registered (Casual)", fontsize=12)
+
+plt.subplot(gs1[2:, 2:])
+sns.boxplot(x='weathersit', y='registered', data=df_day_new, palette="Set2", hue='weathersit', legend=False)
+plt.xlabel("Cuaca", fontsize=12)
+plt.ylabel("Jumlah Penyewa Registered", fontsize=12)
+
+st.pyplot(fig1)
+
+# Visualisasi penyewa sepeda berdasarkan "workingday"
+st.subheader("Jumlah Penyewa Berdasarkan Hari Kerja dan Hari Libur")
+fig2 = plt.figure(figsize=(13, 10))
+gs2 = GridSpec(4, 4, fig2, wspace=0.5, hspace=0.5)
+plt.suptitle("Jumlah Penyewa berdasarkan Hari Kerja dan Hari Libur", fontsize=16)
+
+plt.subplot(gs2[:2, 1:3])
+sns.boxplot(x='workingday', y='cnt', data=df_day_new, palette="rocket", hue='workingday', legend=False)
+plt.xlabel("Hari Kerja (1) vs Hari Libur (0)", fontsize=12)
+plt.ylabel("Jumlah Penyewa", fontsize=12)
+
+plt.subplot(gs2[2:, :2])
+sns.boxplot(x='workingday', y='casual', data=df_day_new, palette="rocket", hue='workingday', legend=False)
+plt.xlabel("Hari Kerja (1) vs Hari Libur (0)", fontsize=12)
+plt.ylabel("Jumlah Penyewa Non-Registered (Casual)", fontsize=12)
+
+plt.subplot(gs2[2:, 2:])
+sns.boxplot(x='workingday', y='registered', data=df_day_new, palette="rocket", hue='workingday', legend=False)
+plt.xlabel("Hari Kerja (1) vs Hari Libur (0)", fontsize=12)
+plt.ylabel("Jumlah Penyewa Registered", fontsize=12)
+
+st.pyplot(fig2)
 
 # Conclusion section
 st.subheader("Kesimpulan")
 st.write("""
-1. Berdasarkan hasil visualisasi data yang telah dilakukan, diperoleh kesimpulan bahwa cuaca memiliki pengaruh signifikan terhadap jumlah peminjaman sepeda. Pada cuaca cerah, jumlah penyewaan sepeda cenderung lebih tinggi, baik untuk pengguna non-registered(casual) maupun pengguna registered. Sebaliknya, pada cuaca hujan salju ringan, jumlah penyewaan sepeda menurun drastis. Dengan demikian, Pengguna non-registered(casual) sangat dipengaruhi oleh kondisi cuaca, sementara pengguna registered cenderung lebih konsisten meskipun dalam cuaca buruk.
-2. Berdasarkan hasil visualisasi data yang telah dilakukan, diperoleh bahwa penggunaan sepeda pada hari kerja dan hari libur menunjukkan perbedaan pola antara pengguna non-registered(casual) dan pengguna registered. Pengguna non-registered(casual) lebih banyak menyewa sepeda pada hari libur, sedangkan pengguna registered lebih sering menyewa sepeda pada hari kerja. Dengan demikian, jumlah peminjam sepeda paling banyak terjadi pada hari kerja untuk pengguna registered, sedangkan bagi pengguna non-registered(casual), hari libur adalah waktu dengan jumlah penyewaan terbanyak.
+1. Visualisasi menunjukkan bahwa cuaca memiliki dampak yang signifikan terhadap jumlah peminjam sepeda.
+2. Terdapat perbedaan jelas antara peminjaman sepeda pada hari kerja dan hari libur.
 """)
 st.caption('Copyright (c), created by Meirida Karisma Putri')
+
+
