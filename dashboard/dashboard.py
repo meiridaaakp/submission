0d1888a32340b101c442b40b95a57e686e4d7f92
meiridaaakp <meirida1652@gmail.com>
2024-10-02 16:29:42 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,15 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import os
-
+from babel.numbers import format_currency
+from matplotlib.gridspec import GridSpec
+%matplotlib inline
 sns.set(style='dark')
 
-# Print the files in the dashboard directory
-print("Files in 'dashboard' directory:", os.listdir("dashboard"))
-
 # Load dataset
-df_day = pd.read_csv("dashboard/day_data.csv")
+df_day = pd.read_csv("day.csv")
 
 # Streamlit configuration
 st.set_page_config(page_title="BIKERS", page_icon="🚴‍♂️", layout="wide")
@@ -44,6 +42,8 @@ st.pyplot(fig)
 
 # Conclusion section
 st.subheader("Kesimpulan")
-st.write("""...""")
+st.write("""
+1. Berdasarkan hasil visualisasi data yang telah dilakukan, diperoleh kesimpulan bahwa cuaca memiliki pengaruh signifikan terhadap jumlah peminjaman sepeda. Pada cuaca cerah, jumlah penyewaan sepeda cenderung lebih tinggi, baik untuk pengguna non-registered(casual) maupun pengguna registered. Sebaliknya, pada cuaca hujan salju ringan, jumlah penyewaan sepeda menurun drastis. Dengan demikian, Pengguna non-registered(casual) sangat dipengaruhi oleh kondisi cuaca, sementara pengguna registered cenderung lebih konsisten meskipun dalam cuaca buruk.
+2. Berdasarkan hasil visualisasi data yang telah dilakukan, diperoleh bahwa penggunaan sepeda pada hari kerja dan hari libur menunjukkan perbedaan pola antara pengguna non-registered(casual) dan pengguna registered. Pengguna non-registered(casual) lebih banyak menyewa sepeda pada hari libur, sedangkan pengguna registered lebih sering menyewa sepeda pada hari kerja. Dengan demikian, jumlah peminjam sepeda paling banyak terjadi pada hari kerja untuk pengguna registered, sedangkan bagi pengguna non-registered(casual), hari libur adalah waktu dengan jumlah penyewaan terbanyak.
+""")
 st.caption('Copyright (c), created by Meirida Karisma Putri')
-
